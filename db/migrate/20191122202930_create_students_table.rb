d72667faad089c75658019f80cec945675a95ab4
Gigi Gamao <mmgamao1@gmail.com>
2019-11-22 20:34:33 +0000

    Automatically backed up by Learn

--- db/migrate/20191122202930_create_students_table.rb
+++ db/migrate/20191122202930_create_students_table.rb
@@ -1,6 +1,6 @@
-class CreateStudentsTable < ActiveRecord::Migration[5.0]
+class Students < ActiveRecord::Migration[5.0]
   def change
-    create_table :students_tables do |t|
+    create_table :students do |t|
       t.string :first_name
       t.string :last_name
     end

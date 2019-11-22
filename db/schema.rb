697ad0ef48065dd1fcfeeae2c113da128991a319
Gigi Gamao <mmgamao1@gmail.com>
2019-11-22 20:35:12 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,11 @@
 
 ActiveRecord::Schema.define(version: 20191122202930) do
 
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
+
   create_table "students_tables", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
